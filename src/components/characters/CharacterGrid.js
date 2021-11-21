eb5ee6b0ca91a2b0d35c91a8497445a47b5bcd76
Joe Zucco <josephzucco@gmail.com>
2021-11-21 18:26:08 -0500

    map data to CharacterItem component

--- src/components/characters/CharacterGrid.js
+++ src/components/characters/CharacterGrid.js
@@ -1,10 +1,12 @@
+import CharacterItem from "./CharacterItem";
+
 const CharacterGrid = ({ items, isLoading }) => {
   return isLoading ? (
     <h1>Loading...</h1>
   ) : (
     <section className='cards'>
       {items.map((item) => (
-        <h1>{item.name}</h1>
+        <CharacterItem key={item.char_id} item={item}></CharacterItem>
       ))}
     </section>
   );
---
+++ src/components/characters/CharacterItem.js
@@ -0,0 +1,10 @@
+
+const CharacterItem = ({ item }) => {
+    return (
+        <div>
+            {item.name}
+        </div>
+    )
+}
+
+export default CharacterItem
