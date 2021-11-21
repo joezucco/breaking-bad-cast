f4b60a2a8a5d5e504793cc957e3e38b695c34180
Joe Zucco <josephzucco@gmail.com>
2021-11-21 18:47:48 -0500

    finish build

---
+++ src/components/ui/Spinner.js
@@ -0,0 +1,12 @@
+import spinner from "../../img/spinner.gif";
+const Spinner = () => {
+  return (
+    <img
+      src={spinner}
+      style={{ width: "100px", margin: "auto", display: "block" }}
+      alt='Loading'
+    />
+  );
+};
+
+export default Spinner;
